04b8883e74219daaf4309223d55aa4ec55d232ef
Brian Rainey <brian@rainey.us>
2018-12-10 18:43:47 -0600

    Break up setup scripts.
    Now there are separate scripts for vim setup and git setup.

---
+++ scripts/Scripts/git-setup.sh
@@ -0,0 +1,8 @@
+if [ "$#" -ne 2 ]; then
+  echo "Usage: setup-git.sh name email"
+  exit
+fi
+git config --global user.name "$1"
+git config --global user.email "$2"
+git config --global push.default simple
+git config --global core.editor "gvim -f"
