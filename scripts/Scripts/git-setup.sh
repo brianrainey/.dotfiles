bee8b0f57a6bcb07beffbdd59d338b75009e48cb
Brian Rainey <brian@rainey.us>
2019-08-11 14:48:10 -0500

    Update git setup with KDE tools. Change Gravatar face script to download image once to ~/Pictures.

--- scripts/Scripts/git-setup.sh
+++ scripts/Scripts/git-setup.sh
@@ -5,4 +5,5 @@ fi
 git config --global user.name "$1"
 git config --global user.email "$2"
 git config --global push.default simple
-git config --global core.editor "gvim -f"
+git config --global core.editor "kate -b"
+git config --global diff.tool "kompare"
