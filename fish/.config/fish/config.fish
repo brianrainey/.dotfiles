ff0930cee2ca5d950e4ea7ed8c6556cc7cb428ad
Brian Rainey <brian@rainey.us>
2020-11-18 06:27:31 -0600

    Fish config for Java, Groovy, Gradle, Node.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,1 +1,6 @@
-set hink "Pudding Monster"
+set -x JAVA_HOME ~/.sdk/jdk-15.0.1
+set -x GROOVY_HOME ~/.sdk/groovy-3.0.6
+set -x GRADLE_HOME ~/.sdk/gradle-6.7.1
+set -x NODE_HOME ~/.sdk/node-v14.15.1
+
+set fish_user_paths $JAVA_HOME/bin $GROOVY_HOME/bin $GRADLE_HOME/bin $NODE_HOME/bin
