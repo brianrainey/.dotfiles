52ad2d0bb2358001e48540d0229a62685ef35b80
Brian Rainey <brian@rainey.us>
2021-01-09 07:34:51 -0600

    Update git difftool to gvimdiff.

--- scripts/Scripts/git-setup.sh
+++ scripts/Scripts/git-setup.sh
@@ -6,4 +6,4 @@ git config --global user.name "$1"
 git config --global user.email "$2"
 git config --global push.default simple
 git config --global core.editor "gvim -f"
-git config --global diff.tool "meld"
+git config --global diff.tool "gvimdiff"
